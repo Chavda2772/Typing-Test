22e4ad23046f8f093a30ceade7f4c519b2ee145e
Chavda2772 <chavdamahesh2772@gmail.com>
2022-10-25 13:20:53 +0530

    under maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>deep nagabhai chavda (It's me DC)</h1>
+      <h1>Typing test site under maintenance. </h1>
     </div>
   );
 }
